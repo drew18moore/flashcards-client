28b2769d6cedd24e102d18f6f81a8ba2ba5f3a9a
Drew <dcm752052@gmail.com>
2023-10-17 21:29:40 -0400

    Created auth slice reducers and custom helper functions.

--- src/store/features/auth/authSlice.ts
+++ src/store/features/auth/authSlice.ts
@@ -1,8 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../..";
 
 export const authSlice = createSlice({
   name: "auth",
-  initialState: { userDTO: null, token: null },
+  initialState: { user: null, token: null },
   reducers: {
+    setCredentials: (state, action) => {
+      const { user, accessToken } = action.payload
+      state.user = user;
+      state.token = accessToken;
+    },
+    logout: (state, action) => {
+      state.user = null;
+      state.token = null;
+    }
   }
 })
+
+export const { setCredentials, logout } = authSlice.actions;
+export default authSlice.reducer;
+
+export const selectCurrentUser = (state: RootState) => state.auth.user;
+export const selectCurrentToken = (state: RootState) => state.auth.token;
