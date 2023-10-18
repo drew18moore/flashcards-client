df8ed7d166e9788b1ce56dd860106d1ec8d8de3e
Drew <dcm752052@gmail.com>
2023-10-17 21:23:11 -0400

    Installed and set up redux toolkit.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
 
 export default function App() {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View className="flex-1 justify-center items-center">
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
---
+++ src/store/features/auth/authSlice.ts
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: { userDTO: null, token: null },
+  reducers: {
+  }
+})
