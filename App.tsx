25bb3aff1f2fe3d250a7422e33e2a8880b0c664b
Drew <dcm752052@gmail.com>
2023-10-18 21:59:32 -0400

    Installed react native stack navigation and other required dependencies. Added auth reducer to store, fixing error.

--- App.tsx
+++ App.tsx
@@ -2,14 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
+import Navigation from "./src/components/Navigation";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View className="flex-1 justify-center items-center">
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Navigation />
     </Provider>
   );
 }
