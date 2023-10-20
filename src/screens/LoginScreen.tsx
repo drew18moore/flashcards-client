171f6cd8f6b9aad42b85cc13ad08b769708b799a
Drew <dcm752052@gmail.com>
2023-10-20 15:58:57 -0400

    Added error handling to onLogin function. Error is displayed conditionally.

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -5,6 +5,7 @@ import { useLoginMutation } from "../store/features/auth/authApiSlice";
 export default function LoginScreen() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const [login] = useLoginMutation();
 
@@ -18,23 +19,28 @@ export default function LoginScreen() {
       setUsername("");
       setPassword("");
       console.log(res);
-    } catch (err) {
-      console.error("ERROR", err);
+    } catch (err: any) {
+      if (err.data.statusCode === 400) {
+        setError("Incorrect username or password");
+      } else {
+        setError("Something went wrong");
+      }
     }
   }
 
   return (
     <SafeAreaView>
       <View className="mx-5 space-y-2 mt-20">
-        <Text className="">LOG IN WITH YOUR USERNAME</Text>
+        <Text>LOG IN WITH YOUR USERNAME</Text>
+        {error && <Text className="bg-red-200 text-red-600 self-center px-2 py-1 overflow-hidden" style={{ borderRadius: 6 }}>{error}</Text>}
         <View className="space-y-2">
           <View>
             <Text className="text-lg font-bold">USERNAME</Text>
-            <TextInput autoCapitalize="none" onChangeText={(text) => setUsername(text)} value={username} placeholder="Enter your username" className="text-lg py-1 px-2 border-b-2" />
+            <TextInput autoCapitalize="none" onChangeText={(text) => setUsername(text)} value={username} placeholder="Enter your username" className="text-lg py-1 px-2 border-b-2 focus:border-b-blue-600" />
           </View>
           <View>
             <Text className="text-lg font-bold">PASSWORD</Text>
-            <TextInput autoCapitalize="none" onChangeText={(text) => setPassword(text)} value={password} placeholder="Enter your password" className="text-lg py-1 px-2 border-b-2" />
+            <TextInput autoCapitalize="none" onChangeText={(text) => setPassword(text)} value={password} placeholder="Enter your password" className="text-lg py-1 px-2 border-b-2 focus:border-b-blue-600" />
           </View>
           <TouchableOpacity onPress={onLogin} className="bg-blue-600 p-3 items-center rounded-md">
             <Text className="text-lg text-white/90 font-bold">Log In</Text>
