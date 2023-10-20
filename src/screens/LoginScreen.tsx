0452b36d02ddd1382860b983499a4c1a9abc0053
Drew <dcm752052@gmail.com>
2023-10-20 15:35:38 -0400

    onLogin function now uses login function from useLoginMutation hook and logs res and logs error if any.

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,12 +1,26 @@
 import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
+import { useLoginMutation } from "../store/features/auth/authApiSlice";
 
 export default function LoginScreen() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const onLogin = () => {
+  const [login] = useLoginMutation();
+
+  const onLogin = async () => {
     console.log(username, password);
+    const formattedUsername = username.toLowerCase().trim();
+
+    try {
+      const res = await login({ username: formattedUsername, password }).unwrap()
+      
+      setUsername("");
+      setPassword("");
+      console.log(res);
+    } catch (err) {
+      console.error("ERROR", err);
+    }
   }
 
   return (
@@ -16,11 +30,11 @@ export default function LoginScreen() {
         <View className="space-y-2">
           <View>
             <Text className="text-lg font-bold">USERNAME</Text>
-            <TextInput autoCapitalize="none" onChangeText={(text) => setUsername(text)} placeholder="Enter your username" className="text-lg py-1 px-2 border-b-2" />
+            <TextInput autoCapitalize="none" onChangeText={(text) => setUsername(text)} value={username} placeholder="Enter your username" className="text-lg py-1 px-2 border-b-2" />
           </View>
           <View>
             <Text className="text-lg font-bold">PASSWORD</Text>
-            <TextInput autoCapitalize="none" onChangeText={(text) => setPassword(text)} placeholder="Enter your password" className="text-lg py-1 px-2 border-b-2" />
+            <TextInput autoCapitalize="none" onChangeText={(text) => setPassword(text)} value={password} placeholder="Enter your password" className="text-lg py-1 px-2 border-b-2" />
           </View>
           <TouchableOpacity onPress={onLogin} className="bg-blue-600 p-3 items-center rounded-md">
             <Text className="text-lg text-white/90 font-bold">Log In</Text>
