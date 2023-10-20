d308f12d28011ed804494a3f491b5386532d54bb
Drew <dcm752052@gmail.com>
2023-10-19 20:52:03 -0400

    Created LoginScreen. Added logic to Navigation.tsx to conditionally render login screen when there is no user in state.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,14 +3,23 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../screens/HomeScreen';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../store/features/auth/authSlice';
+import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
+  const currentUser = useSelector(selectCurrentUser);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        { currentUser === null ? (
+          <Stack.Screen name="login" component={LoginScreen} />
+        ): (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   )
