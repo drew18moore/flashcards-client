21554f77450388094a8de64b66eb22dd4f122aee
Drew <dcm752052@gmail.com>
2023-10-23 00:34:49 -0400

    Fixed issue with persist login.

--- src/contexts/PersistLogin.tsx
+++ src/contexts/PersistLogin.tsx
@@ -1,12 +1,18 @@
-import { ReactNode, useEffect } from 'react'
-import { useGetUserFromTokenQuery } from '../store/features/auth/authApiSlice';
+import { ReactNode, useEffect } from "react";
+import { useGetUserFromTokenQuery } from "../store/features/auth/authApiSlice";
 import * as SecureStorage from "expo-secure-store";
-import { selectCurrentToken, setToken, setUser } from '../store/features/auth/authSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectCurrentToken,
+  setToken,
+  setUser,
+} from "../store/features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function PersistLogin({ children }: { children: ReactNode }) {
   const token = useSelector(selectCurrentToken);
-  // const { data } = useGetUserFromTokenQuery({ skip: !token });
+  const { data, isSuccess } = useGetUserFromTokenQuery(undefined, {
+    skip: !token,
+  });
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,14 +21,19 @@ export default function PersistLogin({ children }: { children: ReactNode }) {
         const token = await SecureStorage.getItemAsync("flashcards-jwt");
         if (token) {
           dispatch(setToken(token));
-          // dispatch(setUser(data?.userDTO || null))
         }
       } catch (err) {
         console.error(err);
       }
-    }
+    };
     retrieveToken();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(setUser(data.userDTO));
+    }
+  }, [isSuccess, data]);
 
   return children;
 }
