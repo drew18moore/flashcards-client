c5b3e9e67466ea8d6daec6a473af9f20b52cc08f
Drew <dcm752052@gmail.com>
2023-10-24 19:03:59 -0400

    Created temp logout btn.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,23 @@
-import { SafeAreaView, ScrollView } from 'react-native'
+import { Button, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import { useGetAllDecksQuery } from '../store/features/deck/deckSlice';
-import { useSelector } from 'react-redux';
-import { selectCurrentUser } from '../store/features/auth/authSlice';
+import { useDispatch } from 'react-redux';
+import { logout } from '../store/features/auth/authSlice';
 import Deck from '../store/features/deck/Deck';
+import * as SecureStorage from "expo-secure-store";
 
 export default function HomeScreen() {
   const { data: decks } = useGetAllDecksQuery();
-  const user = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+
+  const onLogout = async () => {
+    dispatch(logout());
+    await SecureStorage.deleteItemAsync("flashcards-jwt");
+  }
 
   return (
     <SafeAreaView>
+      <Button title='logout' onPress={onLogout} />
       <ScrollView className="h-full px-4" contentContainerStyle={{ rowGap: 5, paddingBottom: 60}}>
         {decks?.map(deck => (
           <Deck key={deck.id} id={deck.id} userId={deck.userId} name={deck.name} isPrivate={deck.isPrivate} createdAt={deck.createdAt} />
--- src/store/features/auth/authSlice.ts
+++ src/store/features/auth/authSlice.ts
@@ -10,7 +10,7 @@ export const authSlice = createSlice({
       state.user = user;
       state.token = token;
     },
-    logout: (state, action) => {
+    logout: (state) => {
       state.user = null;
       state.token = null;
     },
