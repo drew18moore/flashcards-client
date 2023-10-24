0bd877034b51511a76360982ccae126c146ee60c
Drew <dcm752052@gmail.com>
2023-10-24 18:33:14 -0400

    PersistLogin component will now handle expired token by deleting token from storage and setting user and token in state to null.

--- src/contexts/PersistLogin.tsx
+++ src/contexts/PersistLogin.tsx
@@ -10,7 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function PersistLogin({ children }: { children: ReactNode }) {
   const token = useSelector(selectCurrentToken);
-  const { data, isSuccess } = useGetUserFromTokenQuery(undefined, {
+  const { data, isSuccess, isError, error } = useGetUserFromTokenQuery(undefined, {
     skip: !token,
   });
   const dispatch = useDispatch();
@@ -34,6 +34,22 @@ export default function PersistLogin({ children }: { children: ReactNode }) {
       dispatch(setUser(data.userDTO));
     }
   }, [isSuccess, data]);
+
+  useEffect(() => {
+    const removeToken = async () => {
+      try {
+        await SecureStorage.deleteItemAsync("flashcards-jwt");
+        dispatch(setToken(null));
+        dispatch(setUser(null));
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    
+    if (error && "status" in error && error.status === 500) {
+      removeToken();
+    }
+  }, [isError])
 
   return children;
 }
