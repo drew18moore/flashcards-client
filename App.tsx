290047f26a09363cbb8d444ed3cc2d6bb3b466a7
Drew <dcm752052@gmail.com>
2023-10-26 09:43:41 -0400

    Added StatusBar component into App.tsx to set status bar text color to white.

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { Provider } from "react-redux";
 import { store } from "./src/store";
 import Navigation from "./src/components/Navigation";
 import PersistLogin from "./src/contexts/PersistLogin";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <Provider store={store}>
+      <StatusBar />
       <PersistLogin>
         <Navigation />
       </PersistLogin>
