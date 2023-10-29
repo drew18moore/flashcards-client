d7a48e69a3117901277d2b622ff537d32cfdf206
Drew <dcm752052@gmail.com>
2023-10-29 18:04:04 -0400

    ProfileScreen now displays user's username. Currently there is a mock display name and joined date.

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,12 +1,22 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../store/features/auth/authSlice";
 
 const ProfileScreen = () => {
+  const user = useSelector(selectCurrentUser);
+
   return (
     <SafeAreaView>
-      <Text>ProfileScreen</Text>
+      <View className="px-4">
+        <View className="space-y-2">
+          <Text className="font-bold text-xl">Drew</Text>
+          <Text>{user?.username}</Text>
+          <Text>Joined October 2023</Text>
+        </View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
