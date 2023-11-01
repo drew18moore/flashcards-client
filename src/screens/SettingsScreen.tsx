5e7fb3f110c088a25490eaa5ee8fff90dfa2f995
Drew <dcm752052@gmail.com>
2023-10-31 21:25:04 -0400

    Styled SettingsScreen header.

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,23 +1,24 @@
-import { Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { Text, SafeAreaView, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const SettingsScreen = () => {
-
   const navigation = useNavigation();
 
   return (
     <SafeAreaView>
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        className="self-start mx-3 p-2"
-      >
-        <MaterialCommunityIcons name="close" size={25} />
-      </TouchableOpacity>
-      <Text>SettingsScreen</Text>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="self-start mx-3 p-2 absolute z-10"
+        >
+          <MaterialCommunityIcons name="close" size={25} />
+        </TouchableOpacity>
+        <Text className="font-bold text-center p-3">Settings</Text>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SettingsScreen
+export default SettingsScreen;
