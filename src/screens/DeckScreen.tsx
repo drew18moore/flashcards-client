7b1d742c8ca992182b98370236d23e935f26a1b5
Drew <dcm752052@gmail.com>
2023-11-05 23:24:23 -0500

    Added logic in DeckScreen component to select a given deck by id from the decks query. Created back btn.

--- src/screens/DeckScreen.tsx
+++ src/screens/DeckScreen.tsx
@@ -1,14 +1,25 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import React from "react";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useGetAllDecksQuery } from "../store/features/deck/deckSlice";
 
 const DeckScreen = () => {
   const {
     params: { id },
   } = useRoute<any>();
 
+  const { data: decks } = useGetAllDecksQuery();
+  const deck = decks?.filter((deck) => deck.id === id)[0];
+  const navigation = useNavigation();
   return (
     <SafeAreaView>
+      <TouchableOpacity
+        onPress={() => navigation.goBack()}
+        className="self-start mx-3 p-2"
+      >
+        <MaterialCommunityIcons name="arrow-left" size={25} />
+      </TouchableOpacity>
       <Text>{id}</Text>
     </SafeAreaView>
   );
