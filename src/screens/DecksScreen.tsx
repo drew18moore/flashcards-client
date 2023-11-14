0fc6ef45274991c1f14f4cb2150d085bed8e917e
Drew <dcm752052@gmail.com>
2023-11-13 23:10:35 -0500

    DecksScreen now has text 'Decks' as a header. Utilized RefreshControl component to allow users to refetch deck data.

--- src/screens/DecksScreen.tsx
+++ src/screens/DecksScreen.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, ScrollView, View } from "react-native";
+import { RefreshControl, SafeAreaView, ScrollView, Text, View } from "react-native";
 import React from "react";
 import { useGetAllDecksQuery } from "../store/features/deck/deckSlice";
 import Deck from "../store/features/deck/Deck";
@@ -7,17 +7,29 @@ import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 export default function DecksScreen() {
-  const { data: decks } = useGetAllDecksQuery();
+  const { data: decks, refetch } = useGetAllDecksQuery();
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }, []);
 
   return (
     <SafeAreaView>
-      <View className="absolute z-10 bottom-3 right-3">
-        <NewDeckButton onPress={() => navigation.navigate("new-deck")}/>
+      <View className="absolute z-10 bottom-14 right-3">
+        <NewDeckButton onPress={() => navigation.navigate("new-deck")} />
       </View>
+      <Text className="text-lg font-bold px-4 py-2">Decks</Text>
       <ScrollView
         className="h-full px-4"
         contentContainerStyle={{ rowGap: 5, paddingBottom: 60 }}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       >
         {decks?.map((deck) => (
           <Deck
