a75f3f0b841f14b02574e933491d69c8bb1c0820
Drew <dcm752052@gmail.com>
2023-11-18 20:12:38 -0500

    Created header for FlashcardsSettingsScreen.

--- src/screens/FlashcardsSettingsScreen.tsx
+++ src/screens/FlashcardsSettingsScreen.tsx
@@ -1,13 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ParamListBase, useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-type Props = {}
+const FlashcardsSettingsScreen = () => {
 
-const FlashcardsSettingsScreen = (props: Props) => {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
   return (
-    <View>
-      <Text>FlashcardsSettingsScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="self-start mx-2 p-2 absolute z-10 left-0"
+        >
+          <MaterialCommunityIcons name="close" size={25} />
+        </TouchableOpacity>
+        <Text className="font-bold text-center p-3">Options</Text>
+        <TouchableOpacity
+          className="self-start mx-2 p-2 absolute z-10 right-0"
+        >
+          <MaterialCommunityIcons
+            name="check"
+            size={25}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
