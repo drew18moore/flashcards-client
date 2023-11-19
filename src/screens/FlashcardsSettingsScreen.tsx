5a48ff979269fc635c258a9128698d0ab815ce0f
Drew <dcm752052@gmail.com>
2023-11-19 11:17:48 -0500

    Created card orientation inputs on FlashcardsSettingsScreen.

--- src/screens/FlashcardsSettingsScreen.tsx
+++ src/screens/FlashcardsSettingsScreen.tsx
@@ -1,13 +1,14 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { ParamListBase, useNavigation } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { View, Text, TouchableOpacity, Pressable } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const FlashcardsSettingsScreen = () => {
-
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  const [isFrontOrientation, setIsFrontOrientation] = useState(true);
 
   return (
     <SafeAreaView>
@@ -19,17 +20,24 @@ const FlashcardsSettingsScreen = () => {
           <MaterialCommunityIcons name="close" size={25} />
         </TouchableOpacity>
         <Text className="font-bold text-center p-3">Options</Text>
-        <TouchableOpacity
-          className="self-start mx-2 p-2 absolute z-10 right-0"
-        >
-          <MaterialCommunityIcons
-            name="check"
-            size={25}
-          />
+        <TouchableOpacity className="self-start mx-2 p-2 absolute z-10 right-0">
+          <MaterialCommunityIcons name="check" size={25} color="#2563EB" />
         </TouchableOpacity>
       </View>
-    </SafeAreaView>
-  )
-}
 
-export default FlashcardsSettingsScreen
+      <View className="p-5 space-y-1">
+        <Text className="text-lg font-bold">CARD ORIENTATION</Text>
+        <View className="flex-row space-x-2">
+          <Pressable onPress={() => setIsFrontOrientation(true)} className={`border p-2 flex-grow items-center rounded-md ${isFrontOrientation ? "border-blue-600" : ""}`}>
+            <Text className={`font-bold ${isFrontOrientation ? "text-blue-600" : ""}`}>Front</Text>
+          </Pressable>
+          <Pressable onPress={() => setIsFrontOrientation(false)} className={`border p-2 flex-grow items-center rounded-md ${!isFrontOrientation ? "border-blue-600" : ""}`}>
+            <Text className={`font-bold ${!isFrontOrientation ? "text-blue-600" : ""}`}>Back</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default FlashcardsSettingsScreen;
