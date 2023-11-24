cc40a1ca6a220811d3cd34b3b40fca924f8c6296
Drew <dcm752052@gmail.com>
2023-11-23 19:36:02 -0500

    Created inputs for TestConfigScreen.

--- src/screens/TestConfigScreen.tsx
+++ src/screens/TestConfigScreen.tsx
@@ -1,11 +1,97 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Platform } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TextInput } from 'react-native';
+import { Switch } from 'react-native';
+import { useGetAllCardsInDeckQuery } from '../store/features/card/cardSlice';
 
 const TestConfigScreen = () => {
+  const {
+    params: { id },
+  } = useRoute<any>();
+  const navigation = useNavigation();
+
+  const { data: cards } = useGetAllCardsInDeckQuery(id);
+
+  const [numQuestions, setNumQuestions] = useState(cards?.length.toString());
+  const [hasTrueFalse, setHasTrueFalse] = useState(false);
+  const [hasMultipleChoice, setHasMultipleChoice] = useState(false);
+  const [hasWritten, setHasWritten] = useState(false);
+
+  const onNumChange = (value: string) => {
+    console.log(value);
+    if (value.match(/^([1-9]\d*|)$/)) {
+      if (value === "" || parseInt(value) <= cards!.length) 
+        setNumQuestions(value)
+    }
+    
+  }
+
+  const onSubmit = () => {
+    console.log(numQuestions, hasTrueFalse, hasMultipleChoice, hasWritten);
+  }
+
   return (
-    <View>
-      <Text>TestConfigScreen</Text>
-    </View>
+    <SafeAreaView className={Platform.OS === "android" ? "pt-6" : ""}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="self-start mx-2 p-2 absolute z-10 left-0"
+        >
+          <MaterialCommunityIcons name="close" size={25} />
+        </TouchableOpacity>
+      </View>
+
+      <View className='p-5 mt-10'>
+        <Text className='text-lg font-bold'>Set up your test</Text>
+      </View>
+
+      <View className="p-5 space-y-4">
+        <View className='flex-row justify-between items-center'>
+          <View className='flex-row items-center space-x-1'>
+          <Text className="font-bold">Question Count</Text>
+          <Text>(Max {cards?.length})</Text>
+          </View>
+          <TextInput
+            inputMode='numeric'
+            style={{ fontSize: 18 }}
+            onChangeText={onNumChange}
+            value={numQuestions}
+            autoCapitalize="none"
+            className="py-1 w-20 border-b-2 focus:border-b-blue-600"
+          />
+        </View>
+        <View className="flex-row items-center justify-between">
+          <Text className="font-bold">True / False</Text>
+          <Switch
+            value={hasTrueFalse}
+            onValueChange={(value) => setHasTrueFalse(value)}
+            trackColor={{ true: "#2563EB" }}
+          />
+        </View>
+        <View className="flex-row items-center justify-between">
+          <Text className="font-bold">Multiple Choice</Text>
+          <Switch
+            value={hasMultipleChoice}
+            onValueChange={(value) => setHasMultipleChoice(value)}
+            trackColor={{ true: "#2563EB" }}
+          />
+        </View>
+        <View className="flex-row items-center justify-between">
+          <Text className="font-bold">Written</Text>
+          <Switch
+            value={hasWritten}
+            onValueChange={(value) => setHasWritten(value)}
+            trackColor={{ true: "#2563EB" }}
+          />
+        </View>
+        <TouchableOpacity disabled={numQuestions === ""} onPress={onSubmit} className={`${ numQuestions === "" ? "bg-gray-400" : "bg-blue-600"} py-3 rounded-md`}>
+          <Text className='text-white text-center'>Start Test</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
