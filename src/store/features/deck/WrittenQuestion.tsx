4c2a4a71e7c43819e2c580f6f8bad4bdfbbb1171
Drew <dcm752052@gmail.com>
2023-11-29 23:07:11 -0500

    answers state now updates when answering written questions.

--- src/store/features/deck/WrittenQuestion.tsx
+++ src/store/features/deck/WrittenQuestion.tsx
@@ -1,13 +1,16 @@
 import { View, Text } from "react-native";
 import React, { useState } from "react";
 import { TextInput } from "react-native";
+import { selectAnswerResponseByQuestionId, submitAnswer } from "./testSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const WrittenQuestion = ({
   index,
   questionText,
   answer,
 }: WrittenQuestion & { index: number }) => {
-  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
+  const response = useSelector(selectAnswerResponseByQuestionId(index));
   return (
     <View className="flex-1">
       <View className="border p-2 rounded-md h-1/2 justify-center items-center">
@@ -16,8 +19,8 @@ const WrittenQuestion = ({
       <View className="flex-1 justify-center">
         <TextInput
           style={{ fontSize: 18 }}
-          onChangeText={(text) => setValue(text)}
-          value={value}
+          onChangeText={(text) => dispatch(submitAnswer({ questionIndex: index, response: text }))}
+          value={response as string}
           autoCapitalize="none"
           placeholder="Enter the answer"
           className="py-1 px-2 border-b-2 focus:border-b-blue-600"
