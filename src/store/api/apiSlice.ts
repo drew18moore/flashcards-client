b0e92946b2cde2e41e61425e331a1822b3b7e472
Drew <dcm752052@gmail.com>
2023-11-30 23:03:12 -0500

    apiSlice now uses base urls from env file.

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -5,8 +5,8 @@ import { Platform } from "react-native";
 const baseQuery = fetchBaseQuery({
   baseUrl:
     Platform.OS === "android"
-      ? "http://192.168.1.26:8080/api/v1"
-      : "http://192.168.1.26:8080/api/v1",
+      ? process.env.EXPO_PUBLIC_BASE_URL_ANDROID
+      : process.env.EXPO_PUBLIC_BASE_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
