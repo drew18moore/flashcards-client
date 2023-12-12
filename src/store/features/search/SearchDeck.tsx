859298fdbd7732b863d58f917bf86f93c0abd7fa
Drew <dcm752052@gmail.com>
2023-12-11 23:07:59 -0500

    Clicking on SearchDeck will navigate user to 'deck' screen if is user's deck else will navigate user to 'user-deck' screen. Removed new card btn and menu btn from UserDeckScreen.

--- src/store/features/search/SearchDeck.tsx
+++ src/store/features/search/SearchDeck.tsx
@@ -2,12 +2,15 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../auth/authSlice';
 
 const SearchDeck = ({ id, name, userId, isPrivate, numCards, createdAt }: Deck) => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  const user = useSelector(selectCurrentUser);
 
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("user-deck", { id, name, numCards, createdAt })} className='border p-5 rounded-md'>
+    <TouchableOpacity onPress={() => navigation.navigate(`${user?.id === userId ? "deck" : "user-deck"}`, { id, name, numCards, createdAt })} className='border p-5 rounded-md'>
       <Text className='font-bold'>{name}</Text>
       <Text>{numCards === 1 ? `${numCards} card`: `${numCards} cards`}</Text>
     </TouchableOpacity>
