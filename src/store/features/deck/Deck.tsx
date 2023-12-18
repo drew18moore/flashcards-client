3ffe211169434246636229a9f4ae7c8f2adcca82
Drew <dcm752052@gmail.com>
2023-12-17 19:53:08 -0500

    Changed style of Deck component.

--- src/store/features/deck/Deck.tsx
+++ src/store/features/deck/Deck.tsx
@@ -5,11 +5,18 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 const Deck = ({ id, userId, name, isPrivate, createdAt, numCards }: Deck) => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
-  
+
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("deck", { id })} className="border p-5 rounded-md">
-      <Text className='font-bold'>{name}</Text>
-      <Text>{numCards === 1 ? `${numCards} card`: `${numCards} cards`}</Text>
+    <TouchableOpacity
+      onPress={() => navigation.navigate("deck", { id })}
+      className="border-2 border-gray-300 p-5 rounded-md bg-white space-y-1"
+    >
+      <Text className="font-bold">{name}</Text>
+      <View className="bg-blue-100 self-start rounded-full px-2">
+        <Text className="">
+          {numCards === 1 ? `${numCards} card` : `${numCards} cards`}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
